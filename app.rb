90599b22aaee21aa50ee8d49c5d941c2d19e5bf4
Isabella Costabile <costabileisa@gmail.com>
2022-12-23 23:25:30 -0500

    commit tree 951576210392b27b389d03f083d56672ca75acb5

--- app.rb
+++ app.rb
@@ -1,12 +1,16 @@
-# pp [{ id: 1, hello: "World" }, 
-#     { id: 2, hello: "Ruby" }, 
-#     { id: 3, hello: "Moon" }, 
-#     { id: 4, hello: "Learner" }]
+pp [{ id: 1, hello: "World" }, 
+   { id: 2, hello: "Ruby" }, 
+   { id: 3, hello: "Moon" }, 
+   { id: 4, hello: "Learner" }]
 
-# p [1,2,3]
-# puts "Hello World!"
-# print "Pass this test, please."
-# p [1,2,3]
+p [1,2,3]
+puts "Hello World!"
+print "Hello World!"
+print "Hello World!"
+print "Hello World!"
+
+print "Pass this test, please."
+p [1,2,3]
 
 # def add
     
